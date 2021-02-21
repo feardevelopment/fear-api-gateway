4364e7064e7766b0c6b3c472361f82febb001936
Vgeorgee <cracken1111@gmail.com>
2021-02-21 17:20:39 +0100

    cors2

--- start.js
+++ start.js
@@ -11,9 +11,19 @@ brokerNode1.createService({
   mixins: [HTTPServer],
   settings: {
     cors: {
-        methods: ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
-        origin: "*",
-    }
+      // Configures the Access-Control-Allow-Origin CORS header.
+      origin: "*",
+      // Configures the Access-Control-Allow-Methods CORS header. 
+      methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+      // Configures the Access-Control-Allow-Headers CORS header.
+      allowedHeaders: [],
+      // Configures the Access-Control-Expose-Headers CORS header.
+      exposedHeaders: [],
+      // Configures the Access-Control-Allow-Credentials CORS header.
+      credentials: false,
+      // Configures the Access-Control-Max-Age CORS header.
+      maxAge: 3600
+  },
   },
 
   settings: {
@@ -28,7 +38,7 @@ brokerNode1.createService({
           origin: ["http://localhost:3000", "https://localhost:4000"],
           methods: ["GET", "OPTIONS", "POST"],
           credentials: false
-      },
+        },
       }
     ]
   }
